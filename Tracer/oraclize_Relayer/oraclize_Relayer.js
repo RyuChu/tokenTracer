61fd7a2d55833d7d7119fa072668bbc014d02fcb
Leo <shspsleo@gmail.com>
2020-08-12 10:48:13 +0800

    update for PoA--5

--- Tracer/oraclize_Relayer/oraclize_Relayer.js
+++ Tracer/oraclize_Relayer/oraclize_Relayer.js
@@ -2,8 +2,8 @@ const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 const ctContract = require('../contract/tracerCT.json');
 const tracerContract = require('../contract/tokenTracer.json');
-let ctAddress = "0x055b0b9cD31b954FBDD9054e649B10E88399f160";
-let relayer = "0x1aCAF568628d2ce875d19008f969578bd097a28D";
+let ctAddress = "0x0978abc07aE6460fB0a2AD639529119a25ef52d0";
+let relayer = "0x0dB684aa00f0498C7e570127268b829fa61EC7E4";
 main();
 async function main() {
 	setInterval(async function() {
