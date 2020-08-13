6c0022d7f24d9ef5c5ed2ca3ae6baf84fdcbd73e
Leo <shspsleo@gmail.com>
2020-08-13 11:16:40 +0800

    update for PoA--15

--- Tracer/routes/index.js
+++ Tracer/routes/index.js
@@ -4,8 +4,8 @@ const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 const ctContract = require('../contract/tracerCT.json');
 const tracerContract = require('../contract/tokenTracer.json');
-const ctAddress = "0x0978abc07aE6460fB0a2AD639529119a25ef52d0";
-const nowAccount = "0x0dB684aa00f0498C7e570127268b829fa61EC7E4";
+const ctAddress = "0x22712Fe98d71356BB0E7e794b5CACCfB4Facf595";
+const nowAccount = "0xD534F0175105Af376773681862C131aB8F30BA43";
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     res.render('index')
