acff86f25884fad17cd5a72be04a43aa25e70ada
Leo <shspsleo@gmail.com>
2020-08-19 17:45:17 +0800

    .

--- Tracer/routes/index.js
+++ Tracer/routes/index.js
@@ -5,7 +5,7 @@ const web3 = new Web3('http://localhost:8545');
 const ctContract = require('../contract/tracerCT.json');
 const tracerContract = require('../contract/tokenTracer.json');
 const ctAddress = "0x92bCf4656Dcd1B48714abe82c1C4BCFBED87d8c1";
-const nowAccount = "0xFc790e0c5486afF2A97db11b25726b8f274Cd0ed";
+const nowAccount = "0x59B02B57Eee720d2e393036271188c82F11dF621";
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     res.render('index')
