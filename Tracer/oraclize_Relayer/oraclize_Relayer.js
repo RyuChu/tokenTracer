87e412ee3d37cf99b1302183121e35ab170ea24c
Leo <shspsleo@gmail.com>
2020-08-21 10:12:33 +0800

    300txs

--- Tracer/oraclize_Relayer/oraclize_Relayer.js
+++ Tracer/oraclize_Relayer/oraclize_Relayer.js
@@ -2,8 +2,8 @@ const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 const ctContract = require('../contract/tracerCT.json');
 const tracerContract = require('../contract/tokenTracer.json');
-let ctAddress = "0x92bCf4656Dcd1B48714abe82c1C4BCFBED87d8c1";
-let relayer = "0x59B02B57Eee720d2e393036271188c82F11dF621";
+let ctAddress = "0x4600eB1910bc74b188B15dd9ef460Eb33bDdBdFC";
+let relayer = "0x42E9AA5145C579C14430dCCBEfdDd5d2F73c6fB4";
 main();
 async function main() {
     setInterval(async function() {
