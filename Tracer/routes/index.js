ba773aa9fc28d5354e5a5e9242b1acc64be4f76c
Leo <shspsleo@gmail.com>
2020-08-21 10:28:45 +0800

    rollback

--- Tracer/routes/index.js
+++ Tracer/routes/index.js
@@ -4,8 +4,8 @@ const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 const ctContract = require('../contract/tracerCT.json');
 const tracerContract = require('../contract/tokenTracer.json');
-const ctAddress = "0x4600eB1910bc74b188B15dd9ef460Eb33bDdBdFC";
-const nowAccount = "0x42E9AA5145C579C14430dCCBEfdDd5d2F73c6fB4";
+const ctAddress = "0x92bCf4656Dcd1B48714abe82c1C4BCFBED87d8c1";
+const nowAccount = "0x59B02B57Eee720d2e393036271188c82F11dF621";
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     res.render('index')
